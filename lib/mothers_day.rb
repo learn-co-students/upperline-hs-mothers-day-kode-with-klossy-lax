1325ddd226f91c4b3c4b9b9bea26f47e357e553f
savannahluy <savannahqluy@gmail.com>
2016-07-27 15:48:44 +0000

    done

--- lib/mothers_day.rb
+++ lib/mothers_day.rb
@@ -1,6 +1,19 @@
-## Define your method, mothers_day, below. Go through the README and update your method as needed!
+#Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+#1
+def mothers_day
+  "Happy Mother's Day, Mom!"
+end
+puts mothers_day()
 
+#2
+def mothers_name(name)
+   "Happy Mother's Day, #{name}!"
+end
+puts mothers_name ("Beyonce")
 
-## Call your method below - use puts to see it in the terminal!
-
+#3
+def mothers_d(name = "Mom")
+  "Happy Mother's Day, #{name}!"
+end
+puts mothers_d() #(test with both "beyonce" and "")
