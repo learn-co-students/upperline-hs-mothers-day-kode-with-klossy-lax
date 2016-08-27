cd0cb92a5efa774583478f539fa204a4f80285ee
sofia-o <sofia.ongele@ealas.org>
2016-08-27 21:30:27 +0000

    get festive.

--- lib/mothers_day.rb
+++ lib/mothers_day.rb
@@ -1,6 +1,8 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
+def mothers_day(mom= "mom", child= "your favourite child")
+  "happy mother's day, #{mom}! from #{child}."
+end
 
 
 ## Call your method below - use puts to see it in the terminal!
-
+puts mothers_day("beyonce", "blue ivy")
